7af788c05c62483047be61dd33011c6091bd8bc3
nirnayapokhrel <112719814+nirnayapokhrel@users.noreply.github.com>
2023-05-23 22:57:03 +0545

    Add files via upload

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,16 +7,16 @@ importScripts(
   "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
 );
 // // Initialize the Firebase app in the service worker by passing the generated config
-const firebaseConfig = {
-  apiKey: "AIzaSyDCKSgdybt_ibKRUgCEKYjnvDqCOMieFVk",
-  authDomain: "bvdeal.firebaseapp.com",
-  projectId: "bvdeal",
-  storageBucket: "bvdeal.appspot.com",
-  messagingSenderId: "358953099184",
-  appId: "1:358953099184:web:eaad28272603acd4670075",
-  measurementId: "G-S346Z0G8WV"
-}
+const firebaseConfig = { 
+apiKey: "AIzaSyDCKSgdybt_ibKRUgCEKYjnvDqCOMieFVk",
+authDomain: "bvdeal.firebaseapp.com",
+projectId: "bvdeal",
+storageBucket: "bvdeal.appspot.com",
+messagingSenderId: "358953099184",
+appId: "1:358953099184:web:eaad28272603acd4670075",
+measurementId: "G-S346Z0G8WV"
 
+}
 firebase?.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
